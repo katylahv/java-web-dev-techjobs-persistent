d81e7d14c3ae280aa3485d1d8e479dff0cc9116c
Katherine Ross <katylahv@gmail.com>
2020-04-12 17:32:51 -0500

    added Repositorys, implemented in controllers

--- queries.sql
+++ queries.sql
@@ -1,5 +1,7 @@
 ## Part 1: Test it with SQL
 
+SELECT * FROM techjobs.job;
+
 ## Part 2: Test it with SQL
 
 ## Part 3: Test it with SQL
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,29 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @Size(min=1, max=30, message = "location must be between 3 & 30 characters")
+    @NotBlank(message = "location required")
+    private String location;
+
+    public Employer(String location){
+        this.location = location;
+    }
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
